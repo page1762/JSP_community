87d5a0f29c487960c16f1ade0ad22ee1e5b99b3e
page1762 <page1762@gmail.com>
2023-04-13 17:47:44 +0900

    Rq 도입

--- src/main/java/org/example/GugudanServlet.java
+++ src/main/java/org/example/GugudanServlet.java
@@ -16,15 +16,15 @@ public class GugudanServlet extends HttpServlet {
     resp.setCharacterEncoding("UTF-8");
     resp.setContentType("text/html; charset-utf-8");
 
-    req.getParameter("dan");
+    Rq rq = new Rq(req, resp);
 
-    int dan = Integer.parseInt(req.getParameter("dan"));
-    int limit = Integer.parseInt(req.getParameter("limit"));
+    int dan = rq.getIntParam("dan", 9);
+    int limit = rq.getIntParam("limit", 9);
 
-    resp.getWriter().append("<h1>%d단</h1>\n".formatted(dan));
+    rq.appendBody("<h1>%d단</h1>\n".formatted(dan));
 
     for(int i = 1; i <= limit; i++) {
-      resp.getWriter().append("<div>%d * %d = %d</div>\n".formatted(dan, i, dan * i));
+      rq.appendBody("<div>%d * %d = %d</div>\n".formatted(dan, i, dan * i));
     }
   }
 }
